4803963a8cf9d0903e617bc78028fe9c39757a96
PrashantKashid <prashantkashid1990@gmail.com>
2022-09-12 17:14:04 +0530

    dowhile

---
+++ learningJava/src/coreJava/DoWhileLoop.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * @author LSPL313
+ *
+ */
+public class DoWhileLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=0;
+		
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+	}
+
+}
