705d5416abb01a73f7d1883a50cd94b071233d95
PrashantKashid <prashantkashid1990@gmail.com>
2022-09-12 16:35:16 +0530

    infiniteDoWhileCode

---
+++ learningJava/src/coreJava/InfinitiveDoWhileLoop.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * @author LSPL313
+ *
+ */
+public class InfinitiveDoWhileLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		do{
+			System.out.println("InfinitiveDoWhileLoop");
+			System.out.println("Press Ctrl+C to stop");
+		}
+		while(true);	
+		
+	}
+
+}
