5f27f5354d6c01389d7021336972ee50ef051860
PrashantKashid <prashantkashid1990@gmail.com>
2022-09-26 18:35:32 +0530

    ContinueExample

---
+++ learningJava/src/coreJava/ContinueExample.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * @author LSPL313
+ *
+ */
+public class ContinueExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i =0; i<=2; i++) {
+			
+			for(int j=i; j<=5 ;j++) {
+				
+				if(j==4) {
+					continue;
+				}
+				System.out.println(j);
+			}
+		}
+		
+	}
+
+}
